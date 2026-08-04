46a55031683e947e736a010591d07a506b028c56
fabian84pa <66214815+fabian84pa@users.noreply.github.com>
2026-08-05 00:19:53 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,22 +1,13 @@
 import os
 
-# ==========================
-# TELEGRAM
-# ==========================
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv(
+    "TELEGRAM_BOT_TOKEN",
+    ""
+)
 
-# ==========================
-# BOT
-# ==========================
 
-BOT_NAME = "Palermo Mercato Bot"
-
-CHECK_INTERVAL_MINUTES = 10
-
-# ==========================
-# PALERMO
-# ==========================
-
-TEAM_NAME = "Palermo FC"
+TELEGRAM_CHAT_ID = os.getenv(
+    "TELEGRAM_CHAT_ID",
+    ""
+)
