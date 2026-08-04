222dd04a53d4893917abe8c0625fdbe126c67272
fabian84pa <66214815+fabian84pa@users.noreply.github.com>
2026-08-04 23:17:57 +0200

    Create test_x_provider.py

---
+++ test_x_provider.py
@@ -0,0 +1,42 @@
+from providers.x_provider import XProvider
+
+
+def main():
+
+    print("Avvio test XProvider")
+
+    provider = XProvider()
+
+    news = provider.fetch()
+
+    print(
+        f"\nNotizie trovate: {len(news)}"
+    )
+
+    for item in news:
+
+        print("\n====================")
+
+        print(
+            "Titolo:",
+            item.title
+        )
+
+        print(
+            "Fonte:",
+            item.source
+        )
+
+        print(
+            "Link:",
+            item.link
+        )
+
+        print(
+            "Summary:",
+            item.summary
+        )
+
+
+if __name__ == "__main__":
+    main()
